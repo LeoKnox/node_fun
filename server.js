ae9754748757799cd708b1f8c3b678238c8d1cd6
LeoKnox <LeoKnox@users.noreply.github.com>
2019-04-23 18:13:17 -0700

    Add files via upload
    Adds badword filter to remove and prevent words using promises and sync/await.

--- server.js
+++ server.js
@@ -3,28 +3,49 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+var dbUrl = "mongodb://localhost/learning_node"
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
-var messages = [
-    {name: 'cat', message: 'meow'},
-    {name: 'cats', message: 'meows'}
-]
-
-app.get('/messages', (req, res) => {
-    res.send(messages)
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
 })
 
-app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
+})
+
+app.post('/messages', async (req, res) => {
+    var message = new Message(req.body)
+    var savedMessage = await message.save()
+        console.log('saved')
+
+        var censored = await Message.findOne({message: 'badword'})
+            if(censored)
+                await Message.deleteOne({_id: censored.id})
+            else
+                io.emit('message', req.body)
+            res.sendStatus(200)
+        
+        /*.catch((err) => {
+            res.sendStatus(500)
+            return console.error(err)
+        })*/
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, {useNewUrlParser: true}, (err) => {
+    console.log('mongo db connnection', err)
 })
 
 const server = http.listen(3000, () => {
