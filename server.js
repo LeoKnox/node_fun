6a330ac095fcd293e7161e2a2c846296a2920fc9
LeoKnox <LeoKnox@users.noreply.github.com>
2019-04-23 19:08:45 -0700

    Add files via upload
    Added error handling with try, catch and finally

--- server.js
+++ server.js
@@ -23,21 +23,24 @@ app.get('/messages', (req, res) => {
 })
 
 app.post('/messages', async (req, res) => {
-    var message = new Message(req.body)
-    var savedMessage = await message.save()
-        console.log('saved')
 
-        var censored = await Message.findOne({message: 'badword'})
-            if(censored)
-                await Message.deleteOne({_id: censored.id})
-            else
-                io.emit('message', req.body)
-            res.sendStatus(200)
-        
-        /*.catch((err) => {
-            res.sendStatus(500)
-            return console.error(err)
-        })*/
+    try {
+        var message = new Message(req.body)
+        var savedMessage = await message.save()
+            console.log('saved')
+    
+            var censored = await Message.findOne({message: 'badword'})
+                if(censored)
+                    await Message.deleteOne({_id: censored.id})
+                else
+                    io.emit('message', req.body)
+                res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    } finally {
+        console.log('done')
+    }
 })
 
 io.on('connection', (socket) => {
