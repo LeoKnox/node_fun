cf253a595c9bb2b6cc806212fdf8ca3a96459ab5
LeoKnox <LeoKnox@users.noreply.github.com>
2019-04-28 21:49:14 -0700

    Add files via upload
    tdd with mocha, chai and node.

---
+++ mocha/test/tools-spec.js
@@ -0,0 +1,22 @@
+var expect = require("chai").expect;
+var tools = require("../lib/tools");
+
+describe("Tools", function() {
+
+    describe("printName()", function() {
+        it("should print the last name first", function() {
+            var results = tools.printName({ first: "Fluffy", last: "Links"});
+
+            expect(results).to.equal("Links, Fluffy");
+        });
+    });
+
+    describe("loadWiki()", function() {
+        it("Load Abraham Lincoln's wikipedia page", function(done) {
+            tools.loadWiki({first: "Abraham", last: "Lincoln"}, function(html){
+                expect(html).to.be.ok;
+                done();
+            });
+        });
+    })
+});
