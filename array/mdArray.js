a349b1fe01aa1a215b2a066c43d37981f1ad4358
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-20 19:09:08 -0700

    Update mdArray.js

--- array/mdArray.js
+++ array/mdArray.js
@@ -1,9 +1,11 @@
 var menu = [["Salad","Soup","Appetizer"],
-           ["Chicken","Beef","Vegetable"]];
+           ["Chicken","Beef","Vegetable","Pork"],
+           ["Ice Cream", "Fruit"]];
 
 
 for (var i = 0; i < menu.length; i++) {
   for (var j = 0; j < menu[i].length; j++) {
     console.log(menu[i][j]);
   }
+  console.log("new");
 }
