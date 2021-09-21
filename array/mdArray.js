1465f0aefd9a37eb024bb4b7bdf3c2dec30fae0f
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-20 22:42:08 -0700

    Update mdArray.js

--- array/mdArray.js
+++ array/mdArray.js
@@ -2,10 +2,15 @@ var menu = [["Salad","Soup","Appetizer"],
            ["Chicken","Beef","Vegetable","Pork"],
            ["Ice Cream", "Fruit"]];
 
+var find = "Beef";
+
 
 for (var i = 0; i < menu.length; i++) {
   for (var j = 0; j < menu[i].length; j++) {
     console.log(menu[i][j]);
+    if (find == menu[i][j]) {
+      console.log("Found at x " + i + " and " + j);
+    }
   }
   console.log("new");
 }
