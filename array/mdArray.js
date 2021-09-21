8c2be9453157c27222321f68519257827f62bc7e
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-20 19:05:35 -0700

    Create mdArray.js

---
+++ array/mdArray.js
@@ -0,0 +1,9 @@
+var menu = [["Salad","Soup","Appetizer"],
+           ["Chicken","Beef","Vegetable"]];
+
+
+for (var i = 0; i < menu.length; i++) {
+  for (var j = 0; j < menu[i].length; j++) {
+    console.log(menu[i][j]);
+  }
+}
