473da419ebb2ba29de789ae5a0dbb7abb85c205a
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-21 19:01:17 -0700

    Create remove_dupe.js

---
+++ remove_dupe.js
@@ -0,0 +1,32 @@
+function removeDuplicateWords (s) {
+  // your perfect code...
+  // store first in array
+  // run through array until dupe is found
+  // delete dupe
+  key_word = "";
+  del_word = "";
+  start = 0;
+  stop = 0;
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] != " ") {
+      key_word = key_word + s[i];
+    } else {
+      console.log(key_word);
+      for (var j = i; j < s.length; j++) {
+        if (s[j] != " ") {
+          if (start != 0) {
+            start = j;
+          }
+          stop = j;
+          del_word = del_word + s[i];
+        } else if (del_word == key_word) {
+          s.slice(start, stop);
+        }
+      }
+      key_word = "";
+    }
+  }
+  for (var x=0; x < s.length; x++) {
+    console.log(s[x]);
+  }
+}
