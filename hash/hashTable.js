4345a2e730318036011326c91f280001607c65e6
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-23 18:35:09 -0700

    Update hashTable.js

--- hash/hashTable.js
+++ hash/hashTable.js
@@ -2,14 +2,25 @@ class hashTable {
   constructor() {
     this.values = {};
     this.length = 0;
-    this.size = 4;
+    this.size = 0;
   }
   
   calculateHash(key) {
     console.log('HI');
     return key.toString().length % this.size;
   }
+  
+  addHash(key, value) {
+    const hash = this.calculateHash(key);
+    if (!this.values.hasOwnProperty(hash)) {
+      this.values[hash] = {};
+    }
+    if (!this.values[hash].hasOwnProperty(key)) {
+      this.length++;
+    }
+    this.values[hash][key] = value;
+  }
 }
 
 var ht = new hashTable();
-console.log(ht.calculateHash("starts"));
+ht.addHash("One", 8)
