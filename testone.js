880b054757d032fdf2252da313008131cc1e8e6b
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-24 18:14:52 -0700

    Update testone.js

--- testone.js
+++ testone.js
@@ -10,21 +10,8 @@ for (let i = 0; i < one.length; i++) {
       total++;
     } else if  ((total < two.length) && stop) {
       console.log("two");
-    }
-  }
-}
-let one = "one two";
-let two = "done first work";
-let stop = true;
-let total = 0;
-
-for (let i = 0; i < one.length; i++) {
-  for (let j = 0; j < two.length; j++) {
-    if (one[i] === two[j]) {
-      console.log("one");
-      total++;
-    } else if  ((total < two.length) && stop) {
-      console.log("two");
+    } else {
+      stop = false;
     }
   }
 }
