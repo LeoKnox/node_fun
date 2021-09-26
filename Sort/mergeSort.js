a619fbc5f628b4e1ac6f5da7e0088e7065e619c4
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-25 19:31:22 -0700

    Update mergeSort.js

--- Sort/mergeSort.js
+++ Sort/mergeSort.js
@@ -1,6 +1,7 @@
-let one = [3,1];
+let one = [3,1,4,6,5,9,11];
 
 function mergeSort(mArr, x, y) {
+  let newArr = [];
   if ((mArr.length < 3) && (mArr[1] < mArr[0])) {
     let temp = mArr[1];
     mArr[1] = mArr[0];
@@ -10,6 +11,9 @@ function mergeSort(mArr, x, y) {
     return mArr;
   } else {
     console.log("3rd else");
+    newArr += mergeSort(mArr.splice(0, mArr.length/2));
+    newArr += mergeSort(mArr.splice(mArr.length/2+1, mArr.length));
+    return(mArr);
   }
   console.log(mArr);
 }
