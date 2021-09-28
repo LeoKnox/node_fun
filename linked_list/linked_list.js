976881807280a7ea569dafc9e5eafade83330676
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-28 16:44:35 -0700

    Update linked_list.js

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -14,12 +14,27 @@ class Link {
   
   add(value) {
     //console.log(value);
-    var newLink = new Node(value);
-    this.head = newLink;
+    if (this.head == null) {
+      var newLink = new Node(value);
+      this.head = newLink;
+    } else {
+      console.log("loop node");
+      let i = 1000;
+      let runner = this.head;
+      while ((runner.next != null) && (i > 0)) {
+        i--;
+        console.log(runner);
+        runner = runner.next;
+      }
+      var newLink = new Node(value);
+      runner.next = newLink;
+    }
   }
 }
 
 ll = new Link();
 console.log(ll);
 ll.add("blue");
+ll.add("green");
+ll.add("orange");
 console.log(ll);
