35bb6e0b3a5eeda8a154ada26ebf60fbf9465a09
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-01 20:23:06 -0700

    Update roman.js

--- roman.js
+++ roman.js
@@ -1,4 +1,4 @@
-let roman = "XXXIX";
+let roman = "DCCLXXXIX";
 let total = 0;
 
 for (var i = 0; i < roman.length; i++) {
@@ -10,6 +10,7 @@ for (var i = 0; i < roman.length; i++) {
   if (roman.charAt(i) == "X") {
     if (roman.charAt(i+1) == "L") {
       total += 40;
+      i++;
     } else {
       total += 10;
     }
@@ -21,9 +22,9 @@ for (var i = 0; i < roman.length; i++) {
     if (roman.charAt(i+1) == "X") {
       total += 9
       i++;
+    } else {
+      total += 1;
     }
-  } else {
-    //total += 1;
   }
   console.log(total);
 }
