72adc76af8bfbb5ad0f21f586633cc03b061549e
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-01 19:28:55 -0700

    Create roman.js

---
+++ roman.js
@@ -0,0 +1,10 @@
+let roman = "LXVI";
+let total = 0;
+
+for (var i = 0; i < roman.length; i++) {
+  if (roman.charAt(i) == "L") {
+    console.log(50);
+    let prev = roman.charAt(i);
+    console.log(prev);
+  }
+}
