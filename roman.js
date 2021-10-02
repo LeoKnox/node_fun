69d097a1aa8bea17fdb976928dfcbec5cb21b646
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-01 19:51:18 -0700

    Update roman.js

--- roman.js
+++ roman.js
@@ -3,17 +3,18 @@ let total = 0;
 
 for (var i = 0; i < roman.length; i++) {
   if (roman.charAt(i) == "L") {
-    console.log(50);
     if (roman.charAt(i+1) == "X") {
       total += 50;
     }
   }
   if (roman.charAt(i) == "X") {
-    console.log(10);
     if (roman.charAt(i+1) == "L") {
       total += 40;
-    } else {
+    } else if (roman.charAt(i+1) == "I") {
+      total += 9;
+    }  else {
       total += 10;
     }
   }
+  console.log(total);
 }
