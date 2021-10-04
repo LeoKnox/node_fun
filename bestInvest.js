0bc02eefde2d74bb93385a73bd2cb913e6527033
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-03 21:08:57 -0700

    Update bestInvest.js

--- bestInvest.js
+++ bestInvest.js
@@ -1,4 +1,4 @@
-let tS = "aab";
+let tS = "aabbbbbbaaacdddd";
 let curChar = tS[0];
 let curTotal = 1;
 let oA = "";
@@ -7,11 +7,15 @@ for (let i = 1; i < tS.length; i++) {
   if (curChar == tS.charAt(i)) {
     curTotal += 1;
   } else {
-    oA.push += curChar;
+    oA += curChar + curTotal;
     curChar = tS.charAt(i);
-    curTotal = 0;
+    curTotal = 1;
   }
-  console.log(tS.charAt(i));
+  if (i == tS.length - 1) {
+    oA += curChar + curTotal;
+    curChar = tS.charAt(i);
+    curTotal = 1;
+  }
 }
 
 console.log(oA);
