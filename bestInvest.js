659be948b9e88d25f95f1a60a91ae6c616a862c8
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-03 19:19:22 -0700

    Update bestInvest.js

--- bestInvest.js
+++ bestInvest.js
@@ -1,15 +1,19 @@
-let tD = [8, 9, 4];
+let tD = [8, 9, 4, 9, 3, 2, 7, 8];
 let low = tD[0];
+let largest = 0;
 let sum = 0;
 
 for (let i = 1; i < tD.length; i++) {
-  console.log("ch " + low);
   if (low < tD[i]) {
-    console.log(tD[i]);
     sum += tD[i]-low;
+    console.log("s " + sum);
+    if ((tD[i]-low) > largest) {
+      largest = tD[i]-low;
+    }
   } else if (low > tD[i]) {
     low = tD[i];
   }
 }
 
+console.log("L " + largest);
 console.log(sum);
