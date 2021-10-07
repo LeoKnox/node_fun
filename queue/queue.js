d2d6b68c4645e03efbda126e041b6e4dac7a2b7e
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-06 20:59:41 -0700

    Create queue.js

---
+++ queue/queue.js
@@ -0,0 +1,13 @@
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
+
+class Node {
+   constructor (value) {
+     this.value = value;
+     this.next = null;
+   }
+}
