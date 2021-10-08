7cc57cbb17e1f15a69dc71691bf35cd7d74955b1
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-07 20:05:25 -0700

    Update privateRoom.js

--- privateRoom.js
+++ privateRoom.js
@@ -1,7 +1,12 @@
 class Room {
+  #door
+  
   constructor(height, width, door) {
     this.height = height;
     this.width = width;
     this.#door = door;
   }
 }
+
+let level = new Room(5, 5, "red");
+console.log(level.width);
