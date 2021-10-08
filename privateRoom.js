84ae4e20ce565649f6adf099331e43ee21e6c046
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-07 20:23:46 -0700

    Update privateRoom.js

--- privateRoom.js
+++ privateRoom.js
@@ -10,7 +10,16 @@ class Room {
   showDoor() {
     console.log(this.#door);
   }
+  
+  static #showRoom() {
+    return ("width: " + this.width + " height: " + this.height);
+  }
+
+  static printRoom() {
+    console.log(this.#showRoom());
+  }
 }
 
 let level = new Room(5, 5, "red");
 level.showDoor();
+level.printRoom();
