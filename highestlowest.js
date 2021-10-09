c234bc99a511b182cca3760deef29de4f3045467
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-08 19:20:01 -0700

    Update highestlowest.js

--- highestlowest.js
+++ highestlowest.js
@@ -1,8 +1,37 @@
 let prices = [2, 8];
-let lowest = highest = newLow = highLow = 0;
+let newLow = newHigh = gain = 0;
+let lowest = highest = prices[0];
 
 // loop through prices.
 // low/highest prices
 // current prices
 // if new low price is lower then low price start new comparison.
-console.log(lowest + " " + highest + " " + newLow + " " + highLow);
+
+console.log(lowest + " " + highest + " " + newLow + " " + newHigh);
+for (i = 1; i < prices.length; i++) {
+  if ((prices[i] < lowest) && (gain == 0)) {
+    lowest = prices[i];
+  } else if ((prices[i] > highest) && (gain == 0)) {
+    highest = prices[i];
+    gain = highest - lowest;
+  } else if (prices[i] > highest) {
+    highest = prices[i];
+  } else if (prcies[i] < lowest) {
+    if (gain > (newHigh - newLow)) {
+      lowest = pries[i];
+      newLow = lowest;
+      newHigh = highest;
+      lowest = 0;
+      highest = 0;
+      gain = 0;
+    } else {
+      lowest = prices[i];
+    }
+  }
+}
+
+if (newLow == 0) {
+  newLow = lowest;
+  newHigh = highest;
+}
+console.log(newLow + " " + newHigh + " " + (newHigh-newLow));
