b6345fdd8da838c84447ca92b0b6512aba7ccbb0
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-08 18:56:43 -0700

    Create highestlowest.js

---
+++ highestlowest.js
@@ -0,0 +1,8 @@
+let prices = [2, 8];
+let lowest = highest = newLow = highLow = 0;
+
+// loop through prices.
+// low/highest prices
+// current prices
+// if new low price is lower then low price start new comparison.
+console.log(lowest + " " + highest + " " + newLow + " " + highLow);
