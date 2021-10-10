47c8aa8976b6eb0a02d82b788251f6fb8f4243ca
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-09 19:17:54 -0700

    Create rotateSting.js

---
+++ rotateSting.js
@@ -0,0 +1,11 @@
+// take string and convert to array
+// loop to value
+// pop and push  through array
+// convert array back to string
+
+function rotateString(newString, value) {
+  let newArray = newString.split("");
+  console.log(newArray);
+}
+
+rotateString("scare", 1);
