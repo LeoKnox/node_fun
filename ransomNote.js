ba1d53d5f430005a5fc4c9d0bfa22a116b263893
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-10 18:35:38 -0700

    Create ransomNote.js

---
+++ ransomNote.js
@@ -0,0 +1,4 @@
+// create a dictionary of arrays.
+// add word to dictionary with number of occurences as array
+// evaluate the dictionary decrementing or deleting matches
+// return true if words are available
