8c0be8740d775c922ad8d3d82bd680a96e6450bc
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-10 18:44:00 -0700

    Update ransomNote.js

--- ransomNote.js
+++ ransomNote.js
@@ -2,3 +2,13 @@
 // add word to dictionary with number of occurences as array
 // evaluate the dictionary decrementing or deleting matches
 // return true if words are available
+
+let words = ['one', 'two', 'three'];
+let phrase = ['two', 'three'];
+let wordsAvail = {};
+
+for (let i = 0; i < words.length; i++) {
+  wordsAvail[words[i]] = [words[i], 0];
+}
+
+console.log(wordsAvail);
